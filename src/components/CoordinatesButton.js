bba72f4de1805378c02380bc79f77dbf8435d50d
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-04-17 15:40:39 +0100

    This was confusing

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component{
+    constructor(){
+        super();
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = (event) => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>Coord</button>
+        );
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,24 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component{
+    constructor() {
+        super();
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick = (event) => {
+        event.persist();
+        setTimeout(() => {
+            this.props.onDelayedClick(event)
+        }, this.props.delay);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>Click me!</button>
+        );
+    }
+}
